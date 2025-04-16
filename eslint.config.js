4ee196f028812b3edc7cc8b68774d44ab99c5ae2
ederrientedias <eder.dias@rizaasset.com>
2025-04-16 15:03:33 -0300

    chore: add any off

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
+import globals from "globals";
+import js from "@eslint/js";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -19,11 +19,9 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-explicit-any": "off",
     },
   }
 );
