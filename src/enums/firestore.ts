f7b7be4cc4ddc15c5cbb9dc6e47f8f8cc8aa3968
ederrientedias <eder.dias@rizaasset.com>
2025-04-17 09:33:57 -0300

    feat: add firestore enum

---
+++ src/enums/firestore.ts
@@ -0,0 +1,13 @@
+export enum FirestoreCollection {
+  PRODUCTION = "production",
+  DEVELOPMENT = "development",
+}
+
+export enum FirestoreDocument {
+  COMPLIANCE = "compliance",
+}
+
+export enum Field {
+  FILES = "files",
+  LAST_UPLOADS = "last_uploads",
+}
