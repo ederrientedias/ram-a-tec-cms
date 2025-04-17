b092fdf8b7b2ad7934c784032cab7113224db660
ederriente <eder.riente@gmail.com>
2025-04-17 06:56:30 -0300

    feat: add firestore enum

---
+++ src/enums/firestore.enum.ts
@@ -0,0 +1,12 @@
+export enum FirestoreDocuments {
+    ABOUT = "about",
+    COMPLIANCE = "compliance",
+    CONTACTS = "contacts",
+    ETHICAL_CHANEL = "ethical_chagel",
+    HOME = 'home',
+    INFORMATIONAL_TRANPARENCY = 'information_tranparency',
+    INVEST = "invest",
+    INVESTMENT_FUNDS = "investment_funds",
+    SOLUTIONS = "solutions",
+    UTILS = "utils"
+}
