1d31b944d6c49727ca6ded4fbd048445aedc3383
ederriente <eder.riente@gmail.com>
2025-04-17 06:55:01 -0300

    feat: add fund respositorie

---
+++ src/repositories/funds.repository.ts
@@ -0,0 +1,24 @@
+import Firestore from '@/services/firestore';
+import { FirestoreDocuments } from '@/enums/firestore.enum';
+import { IInvestmentFund } from '@/models/firestore';
+
+export const fetchFunds = async (): Promise<IInvestmentFund[]> => {
+  const { data } = await Firestore.getDocumentField(FirestoreDocuments.INVESTMENT_FUNDS);
+  return Array.isArray(data) ? data : [data];
+};
+
+export const fetchDocuments = async (collectionName: string, document: string) => {
+  const response = await Firestore.getCollectionDocument(
+    FirestoreDocuments.INVESTMENT_FUNDS,
+    collectionName,
+    document
+  );
+
+  if (typeof response.data === 'string') {
+    return JSON.parse(response.data);
+  }
+
+  if (Array.isArray(response.data)) {
+    return response.data;
+  }
+};
