b757a0f91fb5b80c1b66c888730f3fee7d4bbd21
ederriente <eder.riente@gmail.com>
2025-04-17 06:56:57 -0300

    feat: auth context

---
+++ src/contexts/AuthContexts.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+
+const AuthContext = createContext({
+    user: null,
+    loading: true,
+    login: () => { },
+    logout: () => { },
+});
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        if (storedUser) {
+            setUser(JSON.parse(storedUser));
+        }
+        setLoading(false);
+    }, []);
+
+    const login: any = (userData: any) => {
+        setUser(userData);
+        localStorage.setItem('user', JSON.stringify(userData));
+    };
+
+    const logout = () => {
+        setUser(null);
+        localStorage.removeItem('user');
+    };
+
+    return (
+        <AuthContext.Provider value={{ user, loading, login, logout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+export const useAuth = () => useContext(AuthContext);
