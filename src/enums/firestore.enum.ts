1a743e33341a2f25ed85fd4a2918e8024f1394f2
ederriente <eder.riente@gmail.com>
2025-04-20 13:38:34 -0300

    style: prettier format

--- src/enums/firestore.enum.ts
+++ src/enums/firestore.enum.ts
@@ -1,12 +1,24 @@
-export enum FirestoreDocuments {
-    ABOUT = "about",
-    COMPLIANCE = "compliance",
-    CONTACTS = "contacts",
-    ETHICAL_CHANEL = "ethical_chagel",
-    HOME = 'home',
-    INFORMATIONAL_TRANPARENCY = 'information_tranparency',
-    INVEST = "invest",
-    INVESTMENT_FUNDS = "investment_funds",
-    SOLUTIONS = "solutions",
-    UTILS = "utils"
+export enum FirestoreCollection {
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development',
+}
+
+export enum FirestoreDocument {
+  ABOUT = 'about',
+  COMPLIANCE = 'compliance',
+  CONTACTS = 'contacts',
+  ETHICAL_CHANEL = 'ethical_chagel',
+  HOME = 'home',
+  INFORMATIONAL_TRANPARENCY = 'information_tranparency',
+  INVEST = 'invest',
+  INVESTMENT_FUNDS = 'investment_funds',
+  SOLUTIONS = 'solutions',
+  UTILS = 'utils',
+  PUBLICATIONS = 'publications',
+}
+
+export enum Field {
+  FILES = 'files',
+  LAST_UPLOADS = 'last_uploads',
+  DATA = 'data',
 }
