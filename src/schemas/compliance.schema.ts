d693db97466824d286ebff9225bf69bdf766d844
ederriente <eder.riente@gmail.com>
2025-04-20 13:38:16 -0300

    style: prettier format

--- src/schemas/compliance.schema.ts
+++ src/schemas/compliance.schema.ts
@@ -1,12 +1,13 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const complianceSchema = z.object({
-  company: z.string().nonempty("Selecione uma empresa"),
+  company: z.string().nonempty('Selecione uma empresa'),
   docName: z
     .string()
-    .nonempty("Nome que será exibido para o documento")
-    .min(1, "O nome do documento é obrigatório"),
-  file: z.any().refine((files) => files?.length > 0, "Arquivo é obrigatório"),
+    .nonempty('Nome que será exibido para o documento')
+    .min(1, 'O nome do documento é obrigatório'),
+  file: z.any().refine((files) => files?.length > 0, 'Arquivo é obrigatório'),
   // .refine((file) => file?.size <= 1024 * 1024, "O arquivo é muito grande. O tamanho máximo permitido é 1MB."),
 });
+
 export type ComplianceSchema = z.infer<typeof complianceSchema>;
