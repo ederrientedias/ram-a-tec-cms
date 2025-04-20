c02ec584dfdaa3abd9cc5c6ab65d532245c6924d
ederriente <eder.riente@gmail.com>
2025-04-20 13:38:02 -0300

    chore: update import

--- src/hooks/firestore/use-funds.ts
+++ src/hooks/firestore/use-funds.ts
@@ -1,11 +1,11 @@
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { fetchFunds } from '@/repositories/funds.repository';
-import { FirestoreDocuments } from '@/enums/firestore.enum';
+import { FirestoreDocument } from '@/enums/firestore.enum';
 import { IInvestmentFund } from '@/models/firestore';
 
 export const useFunds = (): UseQueryResult<IInvestmentFund[], Error> => {
   return useQuery({
-    queryKey: [FirestoreDocuments.INVESTMENT_FUNDS],
+    queryKey: [FirestoreDocument.INVESTMENT_FUNDS],
     queryFn: fetchFunds,
   });
 };
