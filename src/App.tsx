efd211b6d63e84e74019be6ef402d79654ed96e0
ederrientedias <eder.dias@rizaasset.com>
2025-04-25 10:07:02 -0300

    feat: add protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,51 @@
-import { Toaster } from '@/components/ui/toaster';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ProtectedRoute } from '@/components/route/ProtectedRoute';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Toaster as Sonner } from '@/components/ui/sonner';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
-import Fundos from './pages/Funds';
+import { Toaster } from '@/components/ui/toaster';
+
+import DashboardLayout from './components/layout/DashboardLayout';
+import Publicacoes from './pages/Publications';
+import LandingPage from './pages/LandingPage';
 import Portfolios from './pages/Portfolios';
 import Compliance from './pages/Compliance';
-import LandingPage from './pages/LandingPage';
-import Publicacoes from './pages/Publications';
+import Dashboard from './pages/Dashboard';
 import NotFound from './pages/NotFound';
-import DashboardLayout from './components/layout/DashboardLayout';
+import Fundos from './pages/Funds';
+import Login from './pages/Login';
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-
-          {/* Dashboard Routes */}
-          <Route element={<DashboardLayout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/fundos" element={<Fundos />} />
-            <Route path="/portfolios" element={<Portfolios />} />
-            <Route path="/compliance" element={<Compliance />} />
-            <Route path="/landing-page" element={<LandingPage />} />
-            <Route path="/publicacoes" element={<Publicacoes />} />
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route
+              element={
+                <ProtectedRoute>
+                  <DashboardLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/fundos" element={<Fundos />} />
+              <Route path="/portfolios" element={<Portfolios />} />
+              <Route path="/compliance" element={<Compliance />} />
+              <Route path="/landing-page" element={<LandingPage />} />
+              <Route path="/publicacoes" element={<Publicacoes />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
