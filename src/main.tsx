c4b148d3c7746e548e6f35781b5c76e5cbd3fc03
ederrientedias <eder.dias@rizaasset.com>
2025-04-25 10:07:26 -0300

    feat: add msal provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { PublicClientApplication } from '@azure/msal-browser';
+import { msalConfig } from '@/config/msal.config.ts';
+import { MsalProvider } from '@azure/msal-react';
+import { createRoot } from 'react-dom/client';
+import React from 'react';
+
+import App from './App.tsx';
+
+const msalInstance = new PublicClientApplication(msalConfig);
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
+  </React.StrictMode>
+);
