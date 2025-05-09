e919bd1f1d8d94fe8659c81dd77575ad7880eebb
ederrientedias <eder.dias@rizaasset.com>
2025-05-09 17:42:28 -0300

    feat: add comments and implements publication service interface

--- src/services/publications.service.ts
+++ src/services/publications.service.ts
@@ -1,17 +1,31 @@
-import { IPublication } from '@/models/publication.model';
+import { IPublication, IPublicationService } from '@/models/publication.model';
 import publicationRepository from '@/repositories/publication.repository';
 
-class PublicationsService {
+class PublicationsService implements IPublicationService {
+  /**
+   * @description | Obtém todas as publicações
+   * @returns | Promise<IPublication[]> - Array de publicações
+   */
   public async getPublications(): Promise<IPublication[]> {
     return await publicationRepository.get();
   }
 
+  /**
+   * @description | Salva a nova publicação
+   * @param newPublication | Nova publicação
+   * @returns | true se salvou com sucesso
+   */
   public async setPublications(newPublication: IPublication): Promise<boolean> {
     const publications = await this.mergePublications(newPublication);
 
     return await publicationRepository.set(publications);
   }
 
+  /**
+   * @description | Mescla a nova publicação com as publicações existentes
+   * @param newPublication | Nova publicação
+   * @returns | Array de publicações
+   */
   private async mergePublications(newPublication: IPublication): Promise<IPublication[]> {
     const publications = await this.getPublications();
     const mergedPublication = [...publications, { ...newPublication, id: publications.length + 1 }];
