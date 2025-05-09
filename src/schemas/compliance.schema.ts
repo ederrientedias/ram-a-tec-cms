128c2db9097ac9052ae4620a95c0ec36b1bd4108
ederrientedias <eder.dias@rizaasset.com>
2025-05-09 16:49:32 -0300

    feat: update compliance schema

--- src/schemas/compliance.schema.ts
+++ src/schemas/compliance.schema.ts
@@ -1,13 +1,18 @@
 import { z } from 'zod';
 
 export const complianceSchema = z.object({
-  company: z.string().nonempty('Selecione uma empresa'),
+  company: z.string().nonempty('A empresa é obrigatória'),
   docName: z
     .string()
-    .nonempty('Nome que será exibido para o documento')
+    .nonempty('O nome do documento é obrigatório')
     .min(1, 'O nome do documento é obrigatório'),
   file: z.any().refine((files) => files?.length > 0, 'Arquivo é obrigatório'),
   // .refine((file) => file?.size <= 1024 * 1024, "O arquivo é muito grande. O tamanho máximo permitido é 1MB."),
 });
 
 export type ComplianceSchema = z.infer<typeof complianceSchema>;
+export const defaultValues: ComplianceSchema = {
+  company: '',
+  docName: '',
+  file: null,
+};
