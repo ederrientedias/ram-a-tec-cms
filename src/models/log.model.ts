9769e1467c6020fdb7fa2190ce3565c4d34daa27
ederrientedias <eder.dias@rizaasset.com>
2025-05-09 16:50:40 -0300

    feat: add log model

---
+++ src/models/log.model.ts
@@ -0,0 +1,36 @@
+export interface ILogRepository {
+  getLogs<T>(logName: string): Promise<T[] | []>;
+  updateLogs<T>(logName: string, logs: T[]): Promise<boolean>;
+}
+
+export interface ILogService {
+  getLogs<T>(logName: string): Promise<T[] | []>;
+  addLog<T>(logName: string, logRef: T): Promise<boolean>;
+  deleteLog(logName: string, id: string): Promise<boolean>;
+}
+
+export interface ILandingPageLog {
+  id: number;
+  fundName: string;
+  tabName: string;
+  fileName: string;
+  fileType: string;
+  fileMonth: string;
+  fileYear: string;
+  collectionName: string;
+  fundRef: string;
+  docId: string;
+  createAt: number;
+}
+
+export interface IComplianceLog {
+  id: number;
+  company: string;
+  collectionRef: string;
+  bucketName: string;
+  docId: string;
+  docName: string;
+  docType: string;
+  docSize: string;
+  createAt: number;
+}
