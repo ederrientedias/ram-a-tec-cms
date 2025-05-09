a30fb84eb26054f5d77d21eb890d684aa47a55c5
ederrientedias <eder.dias@rizaasset.com>
2025-05-09 16:51:25 -0300

    feat: update methods and add getLogs

--- src/services/logs.service.ts
+++ src/services/logs.service.ts
@@ -1,10 +1,21 @@
-import LogsRepository, { ILog } from '@/repositories/logs/logs.repository';
+import { IComplianceLog, ILandingPageLog, ILogService } from '@/models/log.model';
+import LogsRepository from '@/repositories/logs/logs.repository';
 
-export class LogService {
+export class LogService implements ILogService {
   private logsRepository: typeof LogsRepository;
 
   constructor() {
     this.logsRepository = LogsRepository;
+  }
+
+  /**
+   * @description | Obtém os logs de um documento específico
+   * @param logName | Nome do documento de logs
+   * @returns | Retorna os logs do documento
+   */
+  public async getLogs<T>(logName: string): Promise<T[] | []> {
+    const logs = await this.logsRepository.getLogs<T[]>(logName);
+    return logs;
   }
 
   /**
@@ -13,17 +24,29 @@ export class LogService {
    * @param logRef | Referencia do log
    * @returns | Retorna true se o log foi adicionado com sucesso
    */
-  async addLog(logName: string, logRef: ILog): Promise<boolean> {
-    console.log('LOG:', logName, '|', logRef);
-    const logs = await this.logsRepository.getLogsRef(logName);
+  public async addLog<T>(logName: string, logRef: T): Promise<boolean> {
+    let updatedLogs: T[];
+    let index: number;
+    const logs = await this.logsRepository.getLogs<any>(logName);
 
     if (Array.isArray(logs) && logs.length === 0) {
-      return await this.logsRepository.setLogsRef(logName, [logRef]);
+      return await this.logsRepository.updateLogs<T>(logName, [logRef]);
+    }
+
+    if (Object.prototype.hasOwnProperty.call(logRef, 'docId')) {
+      index = logs.findIndex((log: any) => log.docId === logRef['docId']);
     }
 
     if (Array.isArray(logs) && logs.length > 0) {
-      const logsRef = [logRef, ...logs];
-      return await this.logsRepository.setLogsRef(logName, logsRef);
+      if (index !== -1) {
+        updatedLogs = [...logs];
+        updatedLogs[index] = logRef;
+        return await this.logsRepository.updateLogs(logName, updatedLogs);
+      }
+
+      updatedLogs = [logRef, ...logs];
+
+      return await this.logsRepository.updateLogs(logName, updatedLogs);
     }
   }
 
@@ -33,10 +56,22 @@ export class LogService {
    * @param id | Id do log a ser deletado
    * @returns | Retorna true se o log foi deletado com sucesso
    */
-  async deleteLog(logName: string, id: number): Promise<boolean> {
-    const logs = await this.logsRepository.getLogsRef(logName);
-    const newLogs = logs.filter((log: ILog) => log.id !== id);
-    return await this.logsRepository.setLogsRef(logName, newLogs);
+  public async deleteLog(logName: string, docId: string): Promise<boolean> {
+    const logs = await this.logsRepository.getLogs(logName);
+
+    if (!Array.isArray(logs)) {
+      return false;
+    }
+
+    if (Array.isArray(logs) && logs.length > 0) {
+      const exists = logs.some((log) => log.docId === docId);
+      const index = logs.findIndex((log) => log.docId === docId);
+
+      if (exists && index !== -1) {
+        logs.splice(index, 1);
+        return await this.logsRepository.updateLogs(logName, logs);
+      }
+    }
   }
 }
 export default new LogService();
