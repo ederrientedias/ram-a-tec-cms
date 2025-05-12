bf26d90ebddb2d75e06faef004948864a83a1ca6
ederrientedias <eder.dias@rizaasset.com>
2025-05-12 15:59:03 -0300

    feat: add documents  models

---
+++ src/models/documents.model.ts
@@ -0,0 +1,51 @@
+import { ILandingPageLog } from './log.model';
+
+export interface IDocumentsRepository {
+  getCollectionsMap: (fundName: string) => Promise<ICollectionMap[] | []>;
+  updateCollectionsMap: (fundName: string, collectionMap: ICollectionMap[]) => Promise<boolean>;
+  getFiles: (documentProps: Partial<IDocumentProps>) => Promise<IFile[] | []>;
+  setFiles: (documentProps: IDocumentProps) => Promise<boolean>;
+}
+
+export interface IDocumentService {
+  getCollectionsMap: (fundName: string) => Promise<ICollectionMap[] | []>;
+  updateCollectionsMap: (fundName: string, collectionMap: ICollectionMap[]) => Promise<boolean>;
+  getFiles: (documentProps: Partial<IDocumentProps>) => Promise<IFile[] | []>;
+  updateFile: (documentProps: IDocumentUpdate) => Promise<boolean>;
+  deleteFile: (documentProps: ILandingPageLog) => Promise<boolean>;
+}
+
+export interface ICollectionMap {
+  id: number;
+  displayName: string;
+  collectionName: string;
+  bucketName: string;
+  years: string[];
+  isActive: boolean;
+  isSelected: boolean;
+  isDisabled: boolean;
+  order: number;
+}
+
+export interface IDocumentProps {
+  fundName: string;
+  collectionName: string;
+  year: string;
+  files: IFile[];
+}
+
+export interface IFile {
+  id: number | string;
+  name: string;
+  mes: string;
+  downloadName: string;
+  docId: string;
+  file: string;
+}
+
+export interface IDocumentUpdate {
+  fundName: string;
+  collectionName: string;
+  year: string;
+  file: IFile;
+}
