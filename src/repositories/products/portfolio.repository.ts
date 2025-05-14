1f5056661e33c392bc7697b6e4dce7791d69e6a5
ederrientedias <eder.dias@rizaasset.com>
2025-05-14 09:47:18 -0300

    feat: add portfolio repository

---
+++ src/repositories/products/portfolio.repository.ts
@@ -0,0 +1,44 @@
+import {
+  FirestoreDocument,
+  FirestoreCollection,
+  FundDocument,
+  DocumentCollection,
+  Field,
+} from '@/enums/firestore.enum';
+import { Firestore, CollectionReference, collection, doc, setDoc } from 'firebase/firestore';
+import { IPortfolioRepository, ITable } from '@/models/portfolio.model';
+import { firestoreSite } from '@/config/firebase/firebase-site.config';
+
+class PortfolioRepository implements IPortfolioRepository {
+  protected readonly firestore: Firestore;
+  protected readonly production: CollectionReference;
+  protected readonly development: CollectionReference;
+
+  constructor() {
+    this.firestore = firestoreSite;
+    this.production = collection(this.firestore, FirestoreCollection.PRODUCTION);
+    this.development = collection(this.firestore, FirestoreCollection.DEVELOPMENT);
+  }
+
+  /**
+   * @description | insere os dados da tabela de ativos
+   * @param fundName | nome do fundo
+   * @param data :ITable| dados da Tabela de Ativos
+   * @returns | retorna true se o portfolio foi atualizado com sucesso
+   */
+  public async saveAssetsTable(fundName: string, table: ITable): Promise<boolean> {
+    try {
+      const docRef = doc(this.development, FirestoreDocument.INVESTMENT_FUNDS);
+      const fundRef = collection(docRef, fundName);
+      const documentRef = doc(fundRef, FundDocument.PORTFOLIO);
+      const collectionRef = collection(documentRef, DocumentCollection.LIST_ASSETS);
+      const document = doc(collectionRef, Field.TABLE);
+      await setDoc(document, { headers: table.headers, rows: table.rows }, { merge: true });
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+}
+export default new PortfolioRepository();
