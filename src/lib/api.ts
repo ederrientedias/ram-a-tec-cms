9976db27dc605508f8cef2db9e7bc19d3338aa88
ederrientedias <eder.dias@rizaasset.com>
2025-05-30 18:07:44 -0300

    feat: add create api instance

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,21 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
   timeout: 10000,
 });
+
+export const createApiInstance = (
+  baseURL: string,
+  headers?: Record<string, string>
+): AxiosInstance => {
+  return axios.create({
+    baseURL,
+    timeout: 20000,
+    headers: headers || {
+      'Content-Type': 'application/json',
+    },
+  });
+};
 
 export default api;
