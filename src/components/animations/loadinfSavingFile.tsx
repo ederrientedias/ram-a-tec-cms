be4265863c24e8d058f00881ce65299563c6ebba
ederrientedias <eder.dias@rizaasset.com>
2025-06-06 16:35:45 -0300

    feat: add loading saving file

---
+++ src/components/animations/loadinfSavingFile.tsx
@@ -0,0 +1,16 @@
+import animationData from '@/assets/animations/loading-saving-file.json';
+import Lottie from 'lottie-react';
+
+const LoadingSavingFileAnimation = () => {
+  return (
+    <div className="w-full h-2/3 flex items-center justify-center">
+      <Lottie
+        animationData={animationData}
+        loop={true} // Define se a animação deve repetir
+        autoplay={true} // Define se a animação inicia automaticamente
+        style={{ width: 600, height: 600 }} // Ajuste o tamanho conforme necessário
+      />
+    </div>
+  );
+};
+export default LoadingSavingFileAnimation;
