8e5d34ee4d15f4d7d29667d8c29e0d906a713eb4
ederrientedias <eder.dias@rizaasset.com>
2025-06-06 16:12:14 -0300

    chore: update salesforce service

--- src/services/salesforce.service.ts
+++ src/services/salesforce.service.ts
@@ -1,18 +1,20 @@
+import { IFundResponse, IAnbimaSummaryData } from '@/models/salesforce.model';
 import salesforceRepository from '@/repositories/salesforce.repository';
-import { Data } from '@/models/salesforce.model';
 
 class SalesforceService {
   /**
    * @description Obtém dados do Salesforce para fundos Riza
-   * @returns {Promise<data[] | []>} A resposta da API do Salesforce
+   * @returns {Promise<IFundResponse[] | []>} A resposta da API do Salesforce
    * @throws {Error} Se a solicitação falhar ou a resposta não for bem-sucedida
    */
-  public async getAllRizaFunds(): Promise<Data[] | []> {
-    const response = await salesforceRepository.getAllRizaFunds();
+  public async getAllRizaFunds(): Promise<IFundResponse[] | []> {
+    const response = (await salesforceRepository.getAllRizaFunds()).sort();
     return response;
   }
 
-  public async getRizaFundById(id: string): Promise<any> {}
+  public async getSummaryById(id: string): Promise<IAnbimaSummaryData> {
+    return await salesforceRepository.getAnbimaSummary(id);
+  }
 }
 
 export default new SalesforceService();
