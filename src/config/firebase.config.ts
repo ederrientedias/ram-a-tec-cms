da7402beb7e4072ab220049bfe2641d68ba2b0ef
ederrientedias <eder.dias@rizaasset.com>
2025-06-25 18:59:04 -0300

    chore:  update firestore config

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -5,24 +5,24 @@ import { getAuth } from 'firebase/auth';
 
 // Configuração do primeiro projeto (intranet-e34a3)
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_INTRANET || '',
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_INTRANET || '',
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY_INTRANET || '',
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN_INTRANET || '',
   projectId: 'intranet-e34a3',
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_INTRANET || '',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_INTRANET || '',
-  appId: process.env.REACT_APP_FIREBASE_APP_ID_INTRANET || '',
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID_INTRANET || '',
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET_INTRANET || '',
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_INTRANET || '',
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID_INTRANET || '',
+  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID_INTRANET || '',
 };
 
 // Configuração do segundo projeto (riza-asset-site)
 const firebaseSiteConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY_SITE || '',
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_SITE || '',
+  apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY_SITE || '',
+  authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN_SITE || '',
   projectId: 'riza-asset-site',
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET_SITE || '',
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_SITE || '',
-  appId: process.env.REACT_APP_FIREBASE_APP_ID_SITE || '',
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID_SITE || '',
+  storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET_SITE || '',
+  messagingSenderId: import.meta.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID_SITE || '',
+  appId: import.meta.env.REACT_APP_FIREBASE_APP_ID_SITE || '',
+  measurementId: import.meta.env.REACT_APP_FIREBASE_MEASUREMENT_ID_SITE || '',
 };
 
 // Inicializar os dois aplicativos Firebase com nomes diferentes
@@ -30,14 +30,14 @@ const appIntranet = initializeApp(firebaseConfig, 'intranet');
 const appSite = initializeApp(firebaseSiteConfig, 'site');
 
 // Inicializar serviços para o projeto intranet-e34a3
-const authIntranet = getAuth(appIntranet);
+// const authIntranet = getAuth(appIntranet);
 const firestoreIntranet = getFirestore(appIntranet);
 const storageIntranet = getStorage(appIntranet);
 
 // Inicializar serviços para o projeto riza-asset-site
-const authSite = getAuth(appSite);
+// const authSite = getAuth(appSite);
 const firestoreSite = getFirestore(appSite);
 const storageSite = getStorage(appSite);
 
 // Exportar os serviços
-export { authIntranet, firestoreIntranet, storageIntranet, authSite, firestoreSite, storageSite };
+export { firestoreIntranet, storageIntranet, firestoreSite, storageSite };
