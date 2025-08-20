e181fd2abbe8e46441bb04d42d3c67941eabd529
ederrientedias <eder.dias@rizaasset.com>
2025-08-20 15:59:06 -0300

    feat: update compliance schema

--- src/schemas/compliance.schema.ts
+++ src/schemas/compliance.schema.ts
@@ -1,18 +1,39 @@
 import { z } from 'zod';
 
-export const complianceSchema = z.object({
-  company: z.string().nonempty('A empresa é obrigatória'),
-  docName: z
-    .string()
-    .nonempty('O nome do documento é obrigatório')
-    .min(1, 'O nome do documento é obrigatório'),
-  file: z.any().refine((files) => files?.length > 0, 'Arquivo é obrigatório'),
-  // .refine((file) => file?.size <= 1024 * 1024, "O arquivo é muito grande. O tamanho máximo permitido é 1MB."),
+/**
+ * Schema - Formulário para upload de Arquivo
+ */
+
+export const createfileMetadataSchema = z.object({
+  filename: z.string().nonempty('O nome do arquivo é obrigatório'),
+  fileMedatada: z.any().refine((files) => files?.length > 0, 'Arquivo é obrigatório'),
 });
 
-export type ComplianceSchema = z.infer<typeof complianceSchema>;
-export const defaultValues: ComplianceSchema = {
-  company: '',
-  docName: '',
-  file: null,
+export const updatefileMetadataSchema = z.object({
+  filename: z.string().nonempty('O nome do arquivo é obrigatório'),
+  fileMedatada: z
+    .any()
+    .optional() // Torna o campo opcional
+    .refine((files) => {
+      // Se não foi fornecido, é válido
+      if (!files) return true;
+      // Se foi fornecido, deve ter pelo menos 1 arquivo
+      return files.length > 0;
+    }, 'Arquivo inválido'),
+});
+
+export type CreateFileMetadataSchema = z.infer<typeof createfileMetadataSchema>;
+export type UpdateFileMetadataSchema = z.infer<typeof updatefileMetadataSchema>;
+export const fileMetadataDefaultValues: CreateFileMetadataSchema = {
+  filename: '',
+  fileMedatada: null,
+};
+
+/* SCHEMA - Formulario para criação de Empresa */
+export const createCompanySchema = z.object({
+  companyName: z.string().nonempty('A empresa é obrigatória'),
+});
+export type CreateCompanySchema = z.infer<typeof createCompanySchema>;
+export const createCompanyDefaultValues: CreateCompanySchema = {
+  companyName: '',
 };
