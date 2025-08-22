44870edb8390e7850660d33a09034dbb5be2d957
ederrientedias <eder.dias@rizaasset.com>
2025-08-22 11:35:30 -0300

    feat: add landing page index

---
+++ src/pages/landingpages/index.tsx
@@ -0,0 +1,38 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import CreateLandingPage from './CreateLandingPage';
+import CreateTab from './CreateTab';
+import AddFile from './AddFile';
+
+
+const LandingPage = () => {
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">Documentos da Landing Page</h1>
+      </div>
+
+      <div className="bg-white p-6 rounded-lg shadow">
+        <Tabs defaultValue="addFile" className="w-full">
+          <TabsList>
+            {/* <TabsTrigger value="createLandingPage" disabled={true}>
+              Criar landing page
+            </TabsTrigger> */}
+            <TabsTrigger value="addFile">Adicionar arquivo</TabsTrigger>
+            <TabsTrigger value="createTab">Criar aba</TabsTrigger>
+          </TabsList>
+          <TabsContent value="addFile">
+            <AddFile />
+          </TabsContent>
+          <TabsContent value="createTab">
+            <CreateTab />
+          </TabsContent>
+          <TabsContent value="createLandingPage">
+            <CreateLandingPage />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
+};
+export default LandingPage;
