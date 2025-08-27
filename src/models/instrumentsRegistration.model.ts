116f7fc19a7d26fac49c50d90cfe02ef4542054f
ederrientedias <eder.dias@rizaasset.com>
2025-08-27 19:36:06 -0300

    feat: add instrument registration model

---
+++ src/models/instrumentsRegistration.model.ts
@@ -0,0 +1,22 @@
+export interface IFieldsBlock {
+  id: string;
+  name: string;
+  group: string;
+}
+
+export interface ISelectOption {
+  id: string;
+  options: any[];
+}
+
+export interface IField {
+  id: string;
+  fieldName: string;
+  label: string;
+  type: string;
+  fieldBlockRef: string;
+  placeholder: string | null;
+  collectionData: string | null;
+  optionsRef: string | null;
+  isRequire: boolean;
+}
