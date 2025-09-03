dfb5a54d2f5241b518c5d810d13d41020fd74b71
ederrientedias <eder.dias@rizaasset.com>
2025-09-03 11:43:24 -0300

    feat: add use forms map hook

---
+++ src/hooks/firestore-intranet/use-forms-map.tsx
@@ -0,0 +1,14 @@
+import firestoreService from '@/services/firestore-intranet/firestore.service';
+import { IFormsMap } from '@/models/instrumentsRegistration.model';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { Documents } from '@/enums/firestoreIntranet.enum';
+
+export const useFormsMap = (): UseQueryResult<IFormsMap[], Error> => {
+  return useQuery({
+    queryKey: [Documents.Forms],
+    queryFn: async (): Promise<IFormsMap[]> => await firestoreService.getFormsMap(),
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+    enabled: true,
+  });
+};
