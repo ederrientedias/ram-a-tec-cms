2657bea168df51ca209067d2bf59b3f7f10b92da
ederrientedias <eder.dias@rizaasset.com>
2025-09-18 18:50:03 -0300

    feat: update schema

--- src/schemas/instrument-registration/createFieldsBlock.schema.ts
+++ src/schemas/instrument-registration/createFieldsBlock.schema.ts
@@ -1,16 +1,15 @@
 import { z } from 'zod';
 
-
 /**
  * Schema - Formulário para criar campo
  */
 export const createfieldsBlockSchema = z.object({
   name: z.string().min(1, 'O nome do bloco é obrigatório'),
-  group: z.string().min(1, 'O Grupo é obrigatória'),
+  group: z.array(z.string().min(1)).min(1, 'Selecione pelo menos um grupo'),
 });
 
 export type CreateFieldsBlockSchema = z.infer<typeof createfieldsBlockSchema>;
 export const createFieldsBlockDefaultValues: CreateFieldsBlockSchema = {
   name: '',
-  group: '',
+  group: [],
 };
