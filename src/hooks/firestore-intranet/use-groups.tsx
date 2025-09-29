cfbc7520ed8dabbb43dd0dc96cf6be8cb007adf3
ederrientedias <eder.dias@rizaasset.com>
2025-09-29 13:16:33 -0300

    feat: update use group hook

---
+++ src/hooks/firestore-intranet/use-groups.tsx
@@ -0,0 +1,15 @@
+import firestoreService from '@/services/firestore-intranet/firestore.service';
+import { Documents, SubCollection } from '@/enums/firestoreIntranet.enum';
+import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { IGroup } from '@/models/instruments-registration.model';
+
+export const useGroups = (): UseQueryResult<IGroup[], Error> => {
+  return useQuery({
+    queryKey: [Documents.InstrumentsGroup],
+    queryFn: async (): Promise<IGroup[]> =>
+      await firestoreService.getInstrumentsOrGroups<IGroup>(SubCollection.Groups),
+    refetchOnWindowFocus: false,
+    staleTime: Infinity,
+    enabled: true,
+  });
+};
