2039d132e23be42195d8735f74babc4b482cdd9e
ederrientedias <eder.dias@rizaasset.com>
2025-09-30 10:43:51 -0300

    feat: add formatTimestamp function

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,9 +1,14 @@
-export const formatDate = (date: Date | undefined) => {
+export const formatDate = (date: Date | undefined): string => {
   if (!date) return '';
   return date.toLocaleDateString('pt-BR').toString();
 };
 
-export const isValidDate = (date: Date | undefined) => {
+export const isValidDate = (date: Date | undefined): boolean => {
   if (!date) return false;
   return !isNaN(date.getTime());
+};
+
+export const formatTimestamp = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  return new Intl.DateTimeFormat('pt-BR').format(date);
 };
