0b8fa4d61e5d4cc16752d16a46cf77701893f468
ederrientedias <eder.dias@rizaasset.com>
2025-09-30 10:43:11 -0300

    feat: update use fields hook

--- src/hooks/firestore-intranet/use-fields.tsx
+++ src/hooks/firestore-intranet/use-fields.tsx
@@ -1,11 +1,11 @@
 import firestoreService from '@/services/firestore-intranet/firestore.service';
-import { Documents, SubCollection } from '@/enums/firestoreIntranet.enum';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
 import { IField } from '@/models/instruments-registration.model';
+import { SubCollection } from '@/enums/firestoreIntranet.enum';
 
 export const useFields = (): UseQueryResult<IField[], Error> => {
   return useQuery({
-    queryKey: [Documents.Fields],
+    queryKey: [SubCollection.Fields],
     queryFn: async (): Promise<IField[]> =>
       await firestoreService.getFieldsOrBlocks<IField>(SubCollection.Fields),
     refetchOnWindowFocus: false,
