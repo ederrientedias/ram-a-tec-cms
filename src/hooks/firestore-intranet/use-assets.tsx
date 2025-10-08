787f4f92efb46b192e656c77fff355407f3bc78c
ederrientedias <eder.dias@rizaasset.com>
2025-10-08 19:04:12 -0300

    feat: udpate use asset hook

--- src/hooks/firestore-intranet/use-assets.tsx
+++ src/hooks/firestore-intranet/use-assets.tsx
@@ -1,13 +1,13 @@
 import firestoreService from '@/services/firestore-intranet/firestore.service';
-// import { IFieldsBlock } from '@/models/instrumentsRegistration.model';
 import { useQuery, UseQueryResult } from '@tanstack/react-query';
+import { SubCollection } from '@/enums/firestoreIntranet.enum';
 
-// import { Documents } from '@/enums/firestoreIntranet.enum';
 
 export const useAssets = (): UseQueryResult<any[], Error> => {
   return useQuery({
-    queryKey: ['registred-asset'],
-    queryFn: async (): Promise<any[]> => await firestoreService.getAllRegistredAssets(),
+    queryKey: ['registered-asset'],
+    queryFn: async (): Promise<any[]> =>
+      await firestoreService.getRegisteredAssets(SubCollection.Assets),
     refetchOnWindowFocus: false,
     staleTime: Infinity,
     enabled: true,
