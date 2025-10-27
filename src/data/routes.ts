75fcc04afec59be2e096c043e499b96d1cc1c3ab
ederrientedias <eder.dias@rizaasset.com>
2025-10-27 13:22:52 -0300

    feat: add routes

---
+++ src/data/routes.ts
@@ -0,0 +1,39 @@
+import { FileText, Home, Newspaper, Upload } from 'lucide-react';
+
+export const navItems = [
+  {
+    icon: Home,
+    label: 'Dashboard',
+    href: '/dashboard',
+  },
+  // {
+  //   icon: Database,
+  //   label: 'Fundos',
+  //   href: '/fundos',
+  // },
+  {
+    icon: FileText,
+    label: 'Portfólios',
+    href: '/portfolios',
+  },
+  {
+    icon: Upload,
+    label: 'Compliance',
+    href: '/compliance',
+  },
+  {
+    icon: FileText,
+    label: 'Landing Page',
+    href: '/landing-page',
+  },
+  {
+    icon: Newspaper,
+    label: 'Publicações',
+    href: '/publicacoes',
+  },
+  {
+    icon: FileText,
+    label: 'Sumário',
+    href: '/sumario',
+  },
+];
