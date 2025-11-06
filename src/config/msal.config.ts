0cf4d7ab0e81041c148cf23e517b76cc3d9ecb99
ederrientedias <eder.dias@rizaasset.com>
2025-11-06 11:47:00 -0300

    feat: add config dev

--- src/config/msal.config.ts
+++ src/config/msal.config.ts
@@ -1,12 +1,13 @@
 import { PublicClientApplication } from '@azure/msal-browser';
 
+
 export const msalConfig = {
   auth: {
     clientId: '89fd91ab-9c27-48cd-914d-c7c4f26ec693',
     authority: 'https://login.microsoftonline.com/rizaasset.onmicrosoft.com',
     redirectUri:
-      import.meta.env.VITE_MSAL_REDIRECT_URI_WEB_STAFF_PORTAL || 'https://web-staff-portal.web.app',
-    // 'http://localhost:8080/',
+      // import.meta.env.VITE_MSAL_REDIRECT_URI_WEB_STAFF_PORTAL || 'https://web-staff-portal.web.app',
+      'http://localhost:8080/',
     // import.meta.env.VITE_MSAL_REDIRECT_URI_WEB_STAFF_PORTAL || 'http://localhost:8080/',
   },
   cache: {
