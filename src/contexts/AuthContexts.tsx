01c7019c52dd2cadd9ceb129c7aeb3d7263b78cd
ederrientedias <eder.dias@rizaasset.com>
2025-11-06 11:45:09 -0300

    style: prettier format

--- src/contexts/AuthContexts.tsx
+++ src/contexts/AuthContexts.tsx
@@ -1,39 +1,37 @@
-import { createContext, useState, useEffect, useContext } from 'react'
+import { createContext, useState, useEffect, useContext } from 'react';
 
 const AuthContext = createContext({
-    user: null,
-    loading: true,
-    login: () => { },
-    logout: () => { },
+  user: null,
+  loading: true,
+  login: () => {},
+  logout: () => {},
 });
 
 export function AuthProvider({ children }) {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const storedUser = localStorage.getItem('user');
-        if (storedUser) {
-            setUser(JSON.parse(storedUser));
-        }
-        setLoading(false);
-    }, []);
+  useEffect(() => {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+    setLoading(false);
+  }, []);
 
-    const login: any = (userData: any) => {
-        setUser(userData);
-        localStorage.setItem('user', JSON.stringify(userData));
-    };
+  const login: any = (userData: any) => {
+    setUser(userData);
+    localStorage.setItem('user', JSON.stringify(userData));
+  };
 
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem('user');
-    };
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('user');
+  };
 
-    return (
-        <AuthContext.Provider value={{ user, loading, login, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout }}>{children}</AuthContext.Provider>
+  );
 }
 
 export const useAuth = () => useContext(AuthContext);
