dd4dabc6caddeba4b0e340cdb68c48898d058dd4
ederrientedias <eder.dias@rizaasset.com>
2025-11-12 16:51:53 -0300

    feat: add federation config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,23 @@
+import federation from '@originjs/vite-plugin-federation';
 import { componentTagger } from 'lovable-tagger';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === 'development' && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === 'development' && componentTagger(),
+    federation({
+      name: 'cms',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './CmsApp': './src/App.tsx',
+        './style': './src/index.css',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ].filter(Boolean),
 
   resolve: {
     alias: {
@@ -14,13 +27,18 @@ export default defineConfig(({ mode }) => ({
 
   server: {
     host: true, // permite acesso via LAN (ex: em dispositivos móveis)
-    port: 8080, // define a porta do dev server
+    port: 5002, // define a porta do dev server
     open: true, // abre o navegador automaticamente
   },
-
+  preview: {
+    port: 5002,
+    strictPort: true,
+  },
   build: {
     outDir: 'dist', // saída padrão do Vite
+    target: 'esnext', // ou 'esnext'
     sourcemap: mode === 'development', // ativa sourcemap só em dev
     emptyOutDir: true, // limpa a pasta dist antes da build
+    cssCodeSplit: false,
   },
 }));
