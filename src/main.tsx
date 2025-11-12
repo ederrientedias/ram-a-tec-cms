612a86bd19d6a779c4ccd8028dd700a84800107a
ederrientedias <eder.dias@rizaasset.com>
2025-11-12 16:53:03 -0300

    feat: remove msal provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,18 @@ import './index.css';
 import { PublicClientApplication } from '@azure/msal-browser';
 import { msalConfig } from '@/config/msal.config.ts';
 import { MsalProvider } from '@azure/msal-react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 
 import App from './App.tsx';
 
 const msalInstance = new PublicClientApplication(msalConfig);
-
+const isStandalone = window.location.port === '5002';
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <App />
-    </MsalProvider>
+    {/* <MsalProvider instance={msalInstance}>
+      </MsalProvider> */}
+    <App />
   </React.StrictMode>
 );
