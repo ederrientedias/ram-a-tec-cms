a6acecfe976fb0ed88fa974203f1b122c16fe176
ederrientedias <eder.dias@rizaasset.com>
2026-02-19 17:35:36 -0300

    feat? add create one page

--- src/pages/onePager/createOnePager.tsx
+++ src/pages/onePager/createOnePager.tsx
@@ -1,12 +1,18 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
+import A4Canvas from "./A4Canvas";
+import Lamina from "./lamina";
+
 const createOnePager = () =>  {
   return (
      <div className="space-y-6">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-medium font-serif text-rz-black">One Pager</h1>
       </div>
-       <div className="grid grid-cols-2 gap-4">
-         <div className="bg-rz-beige/40 p-4 rounded-md">
-           <h2 className="text-lg font-medium font-serif text-rz-black">Título do One Pager</h2>
+       <div className="grid grid-cols-12 gap-2 h-full">
+         <div className="col-span-12 w-full h-full max-h-[calc(100vh-120px)]">
+           <ScrollArea className="h-full w-full">
+             <Lamina />
+           </ScrollArea>
          </div>
        </div>
     </div>
