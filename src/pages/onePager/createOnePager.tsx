bc070fe94539cc9e70c0853accbe37d02a0d1df6
ederrientedias <eder.dias@rizaasset.com>
2026-02-20 18:36:45 -0300

    feat: update  components

--- src/pages/onePager/createOnePager.tsx
+++ src/pages/onePager/createOnePager.tsx
@@ -1,21 +1,25 @@
-import { ScrollArea } from "@/components/ui/scroll-area";
-import A4Canvas from "./A4Canvas";
-import Lamina from "./lamina";
+import { ScrollArea } from '@/components/ui/scroll-area';
+import A4Canvas from './A4Canvas';
+import Lamina from './lamina';
+import { PageA4 } from './lamina/PageA4';
+import LaminaEditor from './lamina/LaminaEditor';
 
-const createOnePager = () =>  {
+const createOnePager = () => {
   return (
-     <div className="space-y-6">
+    <div className="space-y-6">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-medium font-serif text-rz-black">One Pager</h1>
       </div>
-       <div className="grid grid-cols-12 gap-2 h-full">
-         <div className="col-span-12 w-full h-full max-h-[calc(100vh-120px)]">
-           <ScrollArea className="h-full w-full">
-             <Lamina />
-           </ScrollArea>
-         </div>
-       </div>
+      <div className="grid grid-cols-12 gap-2 h-full">
+        <div className="col-span-12 w-full h-full max-h-[calc(100vh-120px)]">
+          <ScrollArea className="h-full w-full">
+            {/* <LaminaEditor /> */}
+            {/* <A4Canvas /> */}
+            <Lamina />
+          </ScrollArea>
+        </div>
+      </div>
     </div>
   );
-}
+};
 export default createOnePager;
